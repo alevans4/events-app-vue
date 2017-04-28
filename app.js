263c5aca477a541a3d99d1b1f5f928b8dba6773d
Bert Evans <alevans4@gmail.com>
2017-04-28 11:44:59 -0400

    Fix syntax errors and add gitignore

--- app.js
+++ app.js
@@ -2,35 +2,35 @@
 
 new Vue({
 
-// We want to target the div with an id of 'events'
+  // We want to target the div with an id of 'events'
 
-  el: "#events"
+  el: "#events",
 
-// Here we can register any values or collections that hold data
-// for the application
+  // Here we can register any values or collections that hold data
+  // for the application
 
   data: {
-    event: { name: "", description: "", date: ""},
+    event: { name: "", description: "", date: "" },
     events: []
   },
 
-// Anything within the ready function will run when the application loads
+  // Anything within the ready function will run when the application loads
 
-  mounted: function() {
+  mounted: function () {
 
-// When the application loads, we want to call the method that initializes
-// some data
+    // When the application loads, we want to call the method that initializes
+    // some data
 
     this.fetchEvents();
   },
 
-// Methods we want to use in our application are registered here
+  // Methods we want to use in our application are registered here
 
   methods: {
 
-// We dedicate a method to retrieving and setting some data
+    // We dedicate a method to retrieving and setting some data
 
-    fetchEvents: function() {
+    fetchEvents: function () {
       var events = [
         {
           id: 1,
@@ -54,52 +54,52 @@ new Vue({
         }
       ];
 
-// $set is a convenience method provided by Vue that is similar to pushing
-// data onto an array
+      // $set is a convenience method provided by Vue that is similar to pushing
+      // data onto an array
 
-       this.events = events);
-      },
+      this.events = events;
+    },
 
-  // Adds an event to the existing events array
+    // Adds an event to the existing events array
 
-      addEvent: function(){
-        if(this.event.name){
-          this.events.push(this.event);
-          this.event = { name: "", description: "", date: "" };
-          }
-        }
+    addEvent: function () {
+      if (this.event.name) {
+        this.events.push(this.event);
+        this.event = { name: "", description: "", date: "" };
+      }
+    },
 
-      deleteEvent: function(index) {
-        if(confirm("Are you sure you want to delete this event?")) {
-          //$remove is a Vue convenience method similar to splice
-          var index = this.events.indexOf(index);
-	  this.events.splice(index, 1);
-        }
+    deleteEvent: function (index) {
+      if (confirm("Are you sure you want to delete this event?")) {
+        //$remove is a Vue convenience method similar to splice
+        var index = this.events.indexOf(index);
+        this.events.splice(index, 1);
       }
     }
+  }
 
-// If we had a back end with an events endpoint set up that responds to GET requests
-this.$http.get('api/events').success(function(events) {
-  this.events = events;
-}).error(function(error) {
-  console.log(error);
-});
+  // If we had a back end with an events endpoint set up that responds to GET requests
+  // this.$http.get('api/events').success(function(events) {
+  //   this.events = events;
+  // }).error(function(error) {
+  //   console.log(error);
+  // });
 
-// If we had an endpoint set up that responds to POST requests
-this.$http.post('api/events', this.event).success(function(response) {
-  this.events.push(this.event);
-  console.log("Event added!");
-}).error(function(error) {
-  console.log(error);
-});
+  // // If we had an endpoint set up that responds to POST requests
+  // this.$http.post('api/events', this.event).success(function(response) {
+  //   this.events.push(this.event);
+  //   console.log("Event added!");
+  // }).error(function(error) {
+  //   console.log(error);
+  // });
 
-// We could also delete an event if we had the events endpoint set up to delete data
-this.$http.delete('api/events/' + event.id).success(function(response) {
-  var index = this.events.indexOf(index);
-    this.events.splice(index, 1);
-}).error(function(error) {
-  console.log(error);
-});
+  // // We could also delete an event if we had the events endpoint set up to delete data
+  // this.$http.delete('api/events/' + event.id).success(function(response) {
+  //   var index = this.events.indexOf(index);
+  //     this.events.splice(index, 1);
+  // }).error(function(error) {
+  //   console.log(error);
+  // });
 
 });
 
