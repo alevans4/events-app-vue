1d8fcd13c664e7366e57cc28003fbadc86e17978
Elise <theholistictomboy@gmail.com>
2017-04-28 07:20:13 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ new Vue({
 
 // Anything within the ready function will run when the application loads
 
-  ready: function() {
+  mounted: function() {
 
 // When the application loads, we want to call the method that initializes
 // some data
@@ -57,7 +57,7 @@ new Vue({
 // $set is a convenience method provided by Vue that is similar to pushing
 // data onto an array
 
-       this.$set("events", events);
+       this.events = events);
       },
 
   // Adds an event to the existing events array
@@ -72,9 +72,33 @@ new Vue({
       deleteEvent: function(index) {
         if(confirm("Are you sure you want to delete this event?")) {
           //$remove is a Vue convenience method similar to splice
-          this.events.$remove(index);
+          var index = this.events.indexOf(index);
+	  this.events.splice(index, 1);
         }
       }
     }
 
+// If we had a back end with an events endpoint set up that responds to GET requests
+this.$http.get('api/events').success(function(events) {
+  this.$set('events', events);
+}).error(function(error) {
+  console.log(error);
 });
+
+// If we had an endpoint set up that responds to POST requests
+this.$http.post('api/events', this.event).success(function(response) {
+  this.events.push(this.event);
+  console.log("Event added!");
+}).error(function(error) {
+  console.log(error);
+});
+
+// We could also delete an event if we had the events endpoint set up to delete data
+this.$http.delete('api/events/' + event.id).success(function(response) {
+  this.events.$remove(index);
+}).error(function(error) {
+  console.log(error);
+});
+
+});
+
