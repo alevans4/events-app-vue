91daaf656dc56d39f77bbda67ea73b8de2f64e0b
Elise <theholistictomboy@gmail.com>
2017-04-27 21:17:47 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ new Vue({
 
 // We want to target the div with an id of 'events'
 
-  el: '#events'
+  el: "#events"
 
 // Here we can register any values or collections that hold data
 // for the application
 
   data: {
-    event: { name: '', description: '', date: ''},
+    event: { name: "", description: "", date: ""},
     events: []
 },
 
@@ -34,30 +34,30 @@ new Vue({
       var events = [
         {
           id: 1,
-          name: 'TIFF',
-          description: 'Toronto International Film Festival',
-          date: '2015-09-10'
+          name: "TIFF",
+          description: "Toronto International Film Festival",
+          date: "2015-09-10"
         },
 
         {
           id: 2,
-          name: 'The Martian Premier',
-          description: 'The Martian comes to theatres.',
-          date: '2015-10-02'
+          name: "The Martian Premier",
+          description: "The Martian comes to theatres.",
+          date: "2015-10-02"
         },
 
         {
           id: 3,
-          name: 'SXSW',
-          description: 'Music, film, and interactive festival in Austin, TX.',
-          date: '2016-03-11'
+          name: "SXSW",
+          description: "Music, film, and interactive festival in Austin, TX.",
+          date: "2016-03-11"
         }
       ];
 
 // $set is a convenience method provided by Vue that is similar to pushing
 // data onto an array
 
-       this.$set('events', events);
+       this.$set("events", events);
       },
 
   // Adds an event to the existing events array
@@ -65,7 +65,7 @@ new Vue({
       addEvent: function(){
         if(this.event.name){
           this.events.push(this.event);
-          this.event = { name: '', description: '', date: '' };
+          this.event = { name: "", description: "", date: "" };
           }
         }
       }
